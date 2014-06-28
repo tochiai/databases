21ce8bc9b1c44e9a5c6cb83234314fe5f0763f35
Hack Reactor Students <tyler.a.ochiai@gmail.com>
2014-06-27 17:22:29 -0700

    Set up database schema

--- SQL/schema.sql
+++ SQL/schema.sql
@@ -1,12 +1,94 @@
 CREATE DATABASE chat;
 
+
+
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+
+
+
+
+
+
+-- ---
+-- Globals
+-- ---
+
+-- SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+-- SET FOREIGN_KEY_CHECKS=0;
+
+-- ---
+-- Table 'Users'
+--
+-- ---
+
+DROP TABLE IF EXISTS `Users`;
+
+CREATE TABLE `Users` (
+  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `name` VARCHAR(25) NULL DEFAULT NULL,
+  `id_Rooms` INTEGER NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
 );
 
-/* Create other tables and define schemas for them here! */
+-- ---
+-- Table 'Messages'
+--
+-- ---
+
+DROP TABLE IF EXISTS `Messages`;
+
+CREATE TABLE `Messages` (
+  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `id_Users` INTEGER NULL DEFAULT NULL,
+  `content` VARCHAR(500) NULL DEFAULT NULL,
+  `id_Rooms` INTEGER NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+-- ---
+-- Table 'Rooms'
+--
+-- ---
+
+DROP TABLE IF EXISTS `Rooms`;
+
+CREATE TABLE `Rooms` (
+  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `roomName` VARCHAR(25) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+-- ---
+-- Foreign Keys
+-- ---
+
+ALTER TABLE `Users` ADD FOREIGN KEY (id_Rooms) REFERENCES `Rooms` (`id`);
+ALTER TABLE `Messages` ADD FOREIGN KEY (id_Users) REFERENCES `Users` (`id`);
+ALTER TABLE `Messages` ADD FOREIGN KEY (id_Rooms) REFERENCES `Rooms` (`id`);
+
+-- ---
+-- Table Properties
+-- ---
+
+-- ALTER TABLE `Users` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `Messages` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `Rooms` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- ---
+-- Test Data
+-- ---
+
+-- INSERT INTO `Users` (`id`,`name`,`id_Rooms`) VALUES
+-- ('','','');
+-- INSERT INTO `Messages` (`id`,`id_Users`,`content`,`id_Rooms`) VALUES
+-- ('','','','');
+-- INSERT INTO `Rooms` (`id`) VALUES
+-- ('');
+
+
+
+
 
 
 
